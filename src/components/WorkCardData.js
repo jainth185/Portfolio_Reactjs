93d635a14a1c8b76b3242daa7350a72d2a543cc7
Jainth <jainthasokan185@gmail.com>
2023-03-20 14:58:49 +0530

    asset changes

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,7 @@
 import pro1 from "../assets/project1.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project3.png";
 
 const ProjectCardData = [
     {
@@ -11,18 +12,18 @@ const ProjectCardData = [
     },
     {
         imgsrc: pro2,
-        title: "Pets",
+        title: "Badboot",
         text: "Whether you're looking for the latest fashion trends, high-quality electronics, or unique home decor, we've got you covered. Our vast selection of products has something for everyone, and our competitive prices make it easy to shop within your budget.",
-        view: "https://www.figma.com/file/MW7F99dmJuWTGNssh927gD/pets?t=0Ty8HHNGEBnr12Qn-1"
+        view: "https://www.figma.com/file/nGor93ymi5N4vP9KJBfGHi/SHOE_SITE?node-id=0%3A1&t=C9CWaqs2Ev5XL5G9-1"
     },
     {
         imgsrc: pro3,
-        title: "Transit Card",
+        title: "Clothing",
         text: "Whether you're looking for the latest fashion trends, high-quality electronics, or unique home decor, we've got you covered. Our vast selection of products has something for everyone, and our competitive prices make it easy to shop within your budget.",
         view: "https://www.figma.com/file/cgYxcbcBWvQHMkO1Pl76ts/Transit_card?t=0Ty8HHNGEBnr12Qn-1"
     },
     {
-        imgsrc: pro2,
+        imgsrc: pro4,
         title: "Pets",
         text: "Whether you're looking for the latest fashion trends, high-quality electronics, or unique home decor, we've got you covered. Our vast selection of products has something for everyone, and our competitive prices make it easy to shop within your budget.",
         view: "https://www.figma.com/file/MW7F99dmJuWTGNssh927gD/pets?t=0Ty8HHNGEBnr12Qn-1"
