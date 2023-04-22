f962a3a99ab7128914d0824b43b9d3efaef7033f
Jainth <jainthasokan185@gmail.com>
2023-04-22 14:56:04 +0530

    Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,23 +8,24 @@ const Form = () => {
     // const [email, setEmail] = useState("");
     // const [subject, setSubject]= useState("");
     // const [message, setMessage]=useState("");
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
       
-        const myForm = event.target;
-        const formData = new FormData(myForm);
+    //     const myForm = event.target;
+    //     const formData = new FormData(myForm);
         
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString(),
-        })
-          .then(() => console.log("Form successfully submitted"))
-          .catch((error) => alert(error));
-      };
+    //     fetch("/", {
+    //       method: "POST",
+    //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //       body: new URLSearchParams(formData).toString(),
+    //     })
+    //       .then(() => console.log("Form successfully submitted"))
+    //       .catch((error) => alert(error));
+    //   };
   return (
-    <div className="form" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <form>
+    <div className="form">
+        <form name="contact" method="post" data-netlify="true" onSumbit="submit">
+            <input type="hidden" name="form-name" value="contact"/>
             <label>Your Name</label>
             <input type="text" name="name"></input>
             <label>Email</label>
@@ -33,7 +34,7 @@ const Form = () => {
             <input type="text" name="subject"></input> 
             <label>Message</label>
             <textarea rows="6" placeholder="Type your message here" name="message"/>
-            <button className="btn" onClick={handleSubmit}>Sumbit</button>
+            <button className="btn" type="sumbit">Sumbit</button>
       </form>
     </div>
   )
